1477ef7d1faadb53fcad8511738e3237abd2926e
aanchal <aanwadh@iu.edu>
2019-09-15 16:50:19 -0400

    Changes to include random users
    -Added javascript files
    - Changed the home page for some static information
    -Included all information about person in AboutMe page
    -Changed code to include all data from randomuser.me

---
+++ public/js/AboutMe.js
@@ -0,0 +1,37 @@
+var peopleApp = new Vue({
+  el: '#peopleApp',
+  data: {
+    users: [ {
+      gender: "",
+      name: {
+        title: "",
+        first: "",
+        last: ""
+      },
+      location : {
+        state: ""
+      },
+      email: "",
+      dob: {
+        date:"",
+        age: 0
+      },
+      picture: {
+        large:"",
+        thumbnail:""
+      },
+      nat:""
+    }
+    ]
+    },
+  methods: {
+    fetchUsers() {
+      fetch('https://randomuser.me/api/')
+      .then(function(response) {return response.json()})
+      .then(json => {peopleApp.users = json.results});
+    }
+  },
+  created() {
+    this.fetchUsers();
+  }
+});
